b4f67eb967d7c6dcc5188126fff8065994da2696
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-01-30 16:18:05 -0600

    Proyecto Angular regenerado

--- ProjectTrackerAngular/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: 'pages', pathMatch: "full" },
-    { path: 'login', component: LoginComponent, pathMatch: "full" },
-    { path: 'pages', loadChildren: () => import("./components/layout/layout.module").then(m => m.LayoutModule)},
-    { path: '*', redirectTo: 'pages', pathMatch: "full" }
-];

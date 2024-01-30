9b2e2e1a8e8d4681e8d87716d498d8a502e7158d
danhidalgo09 <danielacr333@hotmail.com>
2024-01-29 18:30:21 -0600

    Layout de Tarea Usuario

--- ProjectTrackerAngular/src/app/components/layout/layout-routing.module.ts
+++ ProjectTrackerAngular/src/app/components/layout/layout-routing.module.ts
@@ -4,6 +4,7 @@ import { LayoutComponent } from './layout.component';
 import { UsuarioComponent } from './pages/usuario/usuario.component';
 import { ProyectoComponent } from './pages/proyecto/proyecto.component';
 import { TareaComponent } from './pages/tarea/tarea.component';
+import { TareaUsuarioComponent } from './pages/tarea-usuario/tarea-usuario.component';
 
 const routes: Routes = [{
   path: '',
@@ -11,6 +12,7 @@ const routes: Routes = [{
   children: [
     { path: 'usuario', component: UsuarioComponent },
     { path: 'proyecto', component: ProyectoComponent },
+    { path: 'tareausuario', component: TareaUsuarioComponent },
     { path: 'tarea', component: TareaComponent }
   ]
 }];

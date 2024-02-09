ea8643735b7c6ec83502f47fbb9258ecf2106e54
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-02-09 02:34:02 -0600

    Fuente de Letra estandarizada

--- ProjectTrackerAngular/src/app/components/layout/layout-routing.module.ts
+++ ProjectTrackerAngular/src/app/components/layout/layout-routing.module.ts
@@ -7,6 +7,8 @@ import { UsuarioComponent } from './usuario/usuario.component';
 import { ProyectoComponent } from './proyecto/proyecto.component';
 import { UsuarioFormularioComponent } from './usuario/usuario-formulario/usuario-formulario.component';
 import { ProyectoFormularioComponent } from './proyecto/proyecto-formulario/proyecto-formulario.component';
+import { TareaFormularioComponent } from './tarea/tarea-formulario/tarea-formulario.component';
+import { HomeComponent } from './home/home.component';
 //import { RoleGuard } from 'src/app/Services/role-guard.service';
 //import { AccesoDenegadoComponent } from './Pages/acceso-denegado/acceso-denegado.component';
 
@@ -14,11 +16,13 @@ const routes: Routes = [{
   path: '',
   component: LayoutComponent,
   children: [
+    { path: '', component: HomeComponent },
     { path: 'usuario', component: UsuarioComponent },
     { path: 'usuario/formulario', component: UsuarioFormularioComponent },
     { path: 'proyecto', component: ProyectoComponent },
     { path: 'proyecto/formulario', component: ProyectoFormularioComponent },
     { path: 'tarea', component: TareaComponent },
+    { path: 'tarea/formulario', component: TareaFormularioComponent },
     { path: 'tareausuario', component: TareaUsuarioComponent }
     //{ path: 'Producto', component: ProductoComponent, canActivate: [RoleGuard], data: {roles: ['Administrador', 'Supervisor'] } },
   ]

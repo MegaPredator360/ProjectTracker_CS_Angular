96b8c66a692b0aa1772b61f814fdec5dcbaf61d9
KeilynMorera <keilyn.morera@ulatina.net>
2024-02-09 14:07:02 -0600

    Tareas

--- ProjectTrackerAngular/src/app/interface/tarea.ts
+++ ProjectTrackerAngular/src/app/interface/tarea.ts
@@ -1,10 +1,14 @@
+import { Usuario } from "./usuario";
+
 export interface Tarea
 {
     tareId: number,
     tareNombre: string,
     tareDescripcion: string,
+    tareFechaInicio: string,
     tareProyId: number,
     tareProyNombre: string,
     tareEstaId: number,
-    tareEstaNombre: string
+    tareEstaNombre: string,
+    tareUsuario: Usuario[]
 }

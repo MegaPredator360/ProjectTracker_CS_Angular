cca2d3a9f404207a30d968a29d98a7e8a40b3cb8
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-03-25 08:58:48 -0600

    Dependencias de React Navigation Agregadas

--- ProjectTrackerReactNative/App.js
+++ ProjectTrackerReactNative/App.js
@@ -1,18 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import LoginScreen from './src/screens/Login';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LoginScreen/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-});

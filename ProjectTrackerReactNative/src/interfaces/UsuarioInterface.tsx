4805a373d2a37afc0c4227de23e8409cdc736c8c
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-03-26 14:41:52 -0600

    Renombremiento de archivos de JS a TSX

---
+++ ProjectTrackerReactNative/src/interfaces/UsuarioInterface.tsx
@@ -0,0 +1,14 @@
+export interface Usuario
+{
+    usuaId: number,
+    usuaCedula: string,
+    usuaNombre: string,
+    usuaUsername: string,
+    usuaCorreo: string,
+    usuaContrasena: string,
+    usuaTelefono: string,
+    usuaDireccion: string,
+    usuaPrimerInicio: number,
+    usuaPermId: number,
+    usuaPermNombre: string
+}

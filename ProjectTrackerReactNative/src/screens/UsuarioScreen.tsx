b4bddfa9b522e8f1200d72237e6c42630a81bb32
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-03-29 22:54:21 -0600

    Servicio para usuarios creados

--- ProjectTrackerReactNative/src/screens/UsuarioScreen.tsx
+++ ProjectTrackerReactNative/src/screens/UsuarioScreen.tsx
@@ -1,11 +1,30 @@
-import { Text, View, StyleSheet } from "react-native"
+import { Text, View, StyleSheet, Button } from "react-native"
+import UsuarioService from "../services/UsuarioService"
+import { useState } from "react"
 
 const UsuarioScreen = () => {
+
+    const [listaUsuario, setListaUsuario] = useState([])
+
+    const obtenerUsuarios = async () => {
+        await UsuarioService.Lista()
+            .then(response => response.json())
+            .then(data => {
+                // Aquí puedes procesar los datos recibidos
+                console.log(data);
+            })
+            .catch(error => {
+                // Manejar errores
+                console.error('Error al realizar la solicitud:', error);
+            })
+        }
+
     return (
         <View style={styles.container}>
             <Text>
                 Usuario Screen
             </Text>
+            <Button title="Obtener Usuarios" onPress={obtenerUsuarios}/>
         </View>
     )
 }
--- ProjectTrackerReactNative/src/services/UsuarioService.tsx
+++ ProjectTrackerReactNative/src/services/UsuarioService.tsx
@@ -1,5 +1,107 @@
-const UsuarioService = () => {
+import { Environment } from "../environments/environment"
+import { InicioSesion } from "../interfaces/InicioSesionInterface"
+import { Usuario } from "../interfaces/UsuarioInterface"
 
+class UsuarioService {
+
+    urlAPI = Environment.endpoint + "Usuario/"
+
+    async Lista(): Promise<Response> {
+        return await fetch(`${this.urlAPI}Lista`, {
+            method: 'GET'
+        })
+    }
+
+    async IniciarSesion(request: InicioSesion): Promise<Response> {
+        return await fetch(`${this.urlAPI}IniciarSesion`, {
+            method: 'POST',
+            body: JSON.stringify({
+                correo: request.correo,
+                contrasena: request.contrasena
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+    }
+
+    async ObtenerUsuario(Id: number): Promise<Response> {
+        return await fetch(`${this.urlAPI}Unico/${Id}`, {
+            method: 'GET'
+        })
+    }
+
+    async Guardar(request: Usuario): Promise<Response> {
+        return await fetch(`${this.urlAPI}Guardar`, {
+            method: 'POST',
+            body: JSON.stringify({
+                usuaId: request.usuaId,
+                usuaCedula: request.usuaCedula,
+                usuaNombre: request.usuaNombre,
+                usuaUsername: request.usuaNombre,
+                usuaCorreo: request.usuaCorreo,
+                usuaContrasena: request.usuaContrasena,
+                usuaTelefono: request.usuaTelefono,
+                usuaDireccion: request.usuaDireccion,
+                usuaPrimerInicio: request.usuaPrimerInicio,
+                usuaPermId: request.usuaPermId,
+                usuaPermNombre: request.usuaPermNombre
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+    }
+
+    async Editar(request: Usuario): Promise<Response> {
+        return await fetch(`${this.urlAPI}Editar`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                usuaId: request.usuaId,
+                usuaCedula: request.usuaCedula,
+                usuaNombre: request.usuaNombre,
+                usuaUsername: request.usuaNombre,
+                usuaCorreo: request.usuaCorreo,
+                usuaContrasena: request.usuaContrasena,
+                usuaTelefono: request.usuaTelefono,
+                usuaDireccion: request.usuaDireccion,
+                usuaPrimerInicio: request.usuaPrimerInicio,
+                usuaPermId: request.usuaPermId,
+                usuaPermNombre: request.usuaPermNombre
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+    }
+
+    async Eliminar(Id: number): Promise<Response> {
+        return await fetch(`${this.urlAPI}Eliminar/${Id}`, {
+            method: 'DELETE'
+        })
+    }
+
+    async CambiarContraseña(request: Usuario): Promise<Response> {
+        return await fetch(`${this.urlAPI}CambiarContrasena`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                usuaId: request.usuaId,
+                usuaCedula: request.usuaCedula,
+                usuaNombre: request.usuaNombre,
+                usuaUsername: request.usuaNombre,
+                usuaCorreo: request.usuaCorreo,
+                usuaContrasena: request.usuaContrasena,
+                usuaTelefono: request.usuaTelefono,
+                usuaDireccion: request.usuaDireccion,
+                usuaPrimerInicio: request.usuaPrimerInicio,
+                usuaPermId: request.usuaPermId,
+                usuaPermNombre: request.usuaPermNombre
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+    }
 }
 
-export default UsuarioService
+export default new UsuarioService

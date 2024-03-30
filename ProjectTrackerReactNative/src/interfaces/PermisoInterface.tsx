89767f212a713d2005efc516aa9d18eea8940a22
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-03-29 23:00:15 -0600

    Interfaces Creadas

---
+++ ProjectTrackerReactNative/src/interfaces/PermisoInterface.tsx
@@ -0,0 +1,5 @@
+export interface Permiso
+{
+    permId: number
+    permNombre: string
+}
---
+++ ProjectTrackerReactNative/src/interfaces/SesionInterface.tsx
@@ -0,0 +1,6 @@
+export interface Sesion
+{
+    usuarioId: string,
+    permisoId: string,
+    primerInicio: string
+}

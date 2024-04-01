5d8da075cbe4b437f041733c32721e2849bc8bdc
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-04-01 08:07:58 -0600

    correcion en muestra de datos

--- ProjectTrackerReactNative/src/screens/UsuarioScreen.tsx
+++ ProjectTrackerReactNative/src/screens/UsuarioScreen.tsx
@@ -1,10 +1,13 @@
-import { Text, View, StyleSheet, Button } from "react-native"
+import { Text, View, StyleSheet, FlatList, Button } from "react-native"
 import UsuarioService from "../services/UsuarioService"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { Usuario } from "../interfaces/UsuarioInterface"
+import MatDivider from "../components/MatDivider/matDivider"
+import MatInput from "../components/MatInput/matInput"
 
 const UsuarioScreen = () => {
 
-    const [listaUsuario, setListaUsuario] = useState([])
+    const [listaUsuario, setListaUsuario] = useState<Usuario[]>([])
 
     // Obtenemos la lista de Usuarios
     const obtenerUsuarios = async () => {
@@ -13,8 +16,8 @@ const UsuarioScreen = () => {
         await UsuarioService.Lista()
             .then(data => {
                 if (data.status) {
-                    // Aquí puedes procesar los datos recibidos
-                    console.log(data.value);
+                    // Aquí se procesan los datos recibidos
+                    setListaUsuario(data.value)
                 }
                 else {
                     console.error(data.msg);
@@ -26,21 +29,54 @@ const UsuarioScreen = () => {
             })
     }
 
+    // Inicializamos la busqueda de usuarios
+    useEffect(() => {
+        obtenerUsuarios()
+    }, [])
+
     return (
-        <View style={styles.container}>
-            <Text>
-                Usuario Screen
-            </Text>
-            <Button title="Obtener Usuarios" onPress={obtenerUsuarios} />
+        <View style={{flex: 1}}>
+            <View style={styles.busquedaContainer}>
+                <Button title="Agregar Usuario"></Button>
+                <MatInput label="Buscar Usuario" />
+            </View>
+            <MatDivider />
+            {/* Se genera la lista de usuarios */}
+            <FlatList
+                style={styles.listaContainer}
+                data={listaUsuario}
+                renderItem={({ item }) => {
+                    return (
+                        <View>
+                            <View style={styles.itemContainer}>
+                                <Text style={styles.itemTitle}>{item.usuaNombre}</Text>
+                                <Text>Cedula: {item.usuaCedula}</Text>
+                                <Text>Correo: {item.usuaCorreo}</Text>
+                                <Text>Permiso: {item.usuaPermNombre}</Text>
+                            </View>
+                            <MatDivider color="#B3B3B3" />
+                        </View>
+                    )
+                }}
+            />
         </View>
     )
+    
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+    busquedaContainer: {    
+        padding: 13
+    },
+    listaContainer: {
+        flexGrow: 1
+    },
+    itemContainer: {
+        padding: 10
+    },
+    itemTitle: {
+        fontSize: 20,
+        fontWeight: 'bold'
     }
 })
 

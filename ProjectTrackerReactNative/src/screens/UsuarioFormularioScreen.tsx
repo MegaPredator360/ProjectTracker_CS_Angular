4c6a7c9aacb2388767fe1dc847d4c49a705a0dd3
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-04-03 13:07:48 -0600

    avances con formulario

--- ProjectTrackerReactNative/src/screens/UsuarioFormularioScreen.tsx
+++ ProjectTrackerReactNative/src/screens/UsuarioFormularioScreen.tsx
@@ -1,45 +1,167 @@
-import { Text, View, StyleSheet, ScrollView, Button } from "react-native"
+import { Text, View, StyleSheet, ScrollView, Button, KeyboardAvoidingView } from "react-native"
 import MatInput from "../components/MatInput/matInput"
 import MatCheckBox from "../components/MatCheckBox/matCheckBox"
-import { useState } from "react";
-import MatDropdown from "../components/MatDropdown/matDropdown";
+import MatButton from "../components/MatButton/matButton";
+import { useEffect, useState } from "react";
+import { Dropdown } from 'react-native-element-dropdown';
+import { Permiso } from "../interfaces/PermisoInterface";
+import { Usuario } from "../interfaces/UsuarioInterface";
+import PermisoService from "../services/PermisoService";
 
-const UsuarioFormularioScreen = () => {
+const UsuarioFormularioScreen = ({ navigation: { goBack } }) => {
 
-    const options = [
-        { label: 'Option 1', value: 'option1' },
-        { label: 'Option 2', value: 'option2' },
-        { label: 'Option 3', value: 'option3' },
-    ];
+    const [listaPermiso, setListaPermiso] = useState<Permiso[]>([])
 
-    const [selectedOption, setSelectedOption] = useState(null);
+    // Obtenemos la lista de Permisos
+    const obtenerPermisos = async () => {
 
-    const handleSelect = (option) => {
-        setSelectedOption(option);
-    };
+        // Llamamos al metodo de lista del servicio de permisos
+        await PermisoService.Lista()
+            .then(data => {
+                if (data.status) {
+
+                    // Aquí se procesan los datos recibidos
+                    setListaPermiso(data.value)
+                }
+                else {
+                    console.error(data.msg);
+                }
+            })
+            .catch(error => {
+                // Manejar errores
+                console.error(error);
+            })
+    }
+
+    // Inicializamos la busqueda de permisos
+    useEffect(() => {
+        obtenerPermisos()
+    }, [])
+
+    const [isFocus, setIsFocus] = useState(false);
+
+    // Valores del formulario
+    const [nombre, setNombre] = useState('')
+    const [cedula, setCedula] = useState('')
+    const [correo, setCorreo] = useState('')
+    const [username, setUsername] = useState('')
+    const [telefono, setTelefono] = useState('')
+    const [direccion, setDireccion] = useState('')
+    const [contra, setContra] = useState('')
+    const [confir, setConfir] = useState('')
+    const [permiso, setPermiso] = useState(0)
+    const [primerInicio, setPrimerInicio] = useState(false)
+
+    // Ocultar Contraseña
+    const [contraOcultar, setContraOcultar] = useState(true);
+    const [confirOcultar, setConfirOcultar] = useState(true);
+
+    const guardarActualizarUsuario = () => {
+        const usuario: Usuario = {
+            usuaId: 0,
+            usuaNombre: nombre,
+            usuaCedula: cedula,
+            usuaCorreo: correo,
+            usuaUsername: username,
+            usuaTelefono: telefono,
+            usuaDireccion: direccion,
+            usuaContrasena: contra,
+            usuaPermId: permiso,
+            usuaPermNombre: "",
+            usuaPrimerInicio: primerInicio ? 1 : 0
+        }
+
+        console.log(usuario)
+    }
 
     return (
         <View style={styles.container}>
-            <ScrollView style={styles.scrollContainer}>
-                <MatInput label="Nombre Completo" />
-                <MatInput label="Número de Cedula" />
-                <MatInput label="Correo Electronico" />
-                <MatInput label="Username" />
-                <MatInput label="Telefono" />
-                <MatInput label="Dirección" />
-                <MatInput label="Contraseña" />
-                <MatInput label="Confirmar Contraseña" />
-                <MatDropdown options={options} onSelect={handleSelect} />
-                {selectedOption && (
-                    <View style={styles.selectedOption}>
-                        <Text>Selected Option: {selectedOption.label}</Text>
-                    </View>
-                )}
-                <MatCheckBox label="Cambiar Contraseña al Inicio de Sesion" />
-                <View style={styles.accionContainer}>
-                    <Button title="Volver" />
-                    <Button title="Agregar Usuario" />
+            <ScrollView style={styles.scrollContainer} keyboardShouldPersistTaps="always">
+                <MatInput
+                    label="Nombre Completo"
+                    onChangeText={(text) => setNombre(text)}
+                    value={nombre}
+                />
+                <MatInput
+                    label="Número de Cedula"
+                    entryType="numeric"
+                    onChangeText={(text) => setCedula(text)}
+                    value={cedula}
+                />
+                <MatInput
+                    label="Correo Electronico"
+                    onChangeText={(text) => setCorreo(text)}
+                    value={correo}
+                />
+                <MatInput
+                    label="Username"
+                    onChangeText={(text) => setUsername(text)}
+                    value={username}
+                />
+                <MatInput
+                    label="Telefono"
+                    entryType="numeric"
+                    onChangeText={(text) => setTelefono(text)}
+                    value={telefono}
+                />
+                <MatInput
+                    label="Dirección"
+                    onChangeText={(text) => setDireccion(text)}
+                    value={direccion}
+                />
+                <MatInput
+                    label="Contraseña"
+                    buttonIcon={contraOcultar ? "eye-outline" : "eye-off-outline"}
+                    buttonIconOnPress={() => contraOcultar ? setContraOcultar(false) : setContraOcultar(true)}
+                    hideText={contraOcultar ? true : false}
+                    onChangeText={(text) => setContra(text)}
+                    value={contra}
+                />
+                <MatInput
+                    label="Confirmar Contraseña"
+                    buttonIcon={confirOcultar ? "eye-outline" : "eye-off-outline"}
+                    buttonIconOnPress={() => confirOcultar ? setConfirOcultar(false) : setConfirOcultar(true)}
+                    hideText={confirOcultar ? true : false}
+                    onChangeText={(text) => setConfir(text)}
+                    value={confir}
+                />
+                <Dropdown
+                    style={[styles.dropdown, isFocus && { borderColor: '#8E7CC3', backgroundColor: '#E0E0E0' }]}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    inputSearchStyle={styles.inputSearchStyle}
+                    iconStyle={styles.iconStyle}
+                    data={listaPermiso}
+                    maxHeight={300}
+                    label="Permiso"
+                    labelField="permNombre"
+                    valueField="permId"
+                    value={permiso.toString()}
+                    onFocus={() => setIsFocus(true)}
+                    onBlur={() => setIsFocus(false)}
+                    onChange={item => {
+                        setPermiso(item.permId);
+                        setIsFocus(false);
+                    }}
+                />
+                <View style={{ marginBottom: 18 }}>
+                    <MatCheckBox 
+                    label="Cambiar Contraseña al Inicio de Sesion" 
+                    onChangeCheck={(check) => setPrimerInicio(check)}
+                    />
                 </View>
+                <MatButton
+                    title="Volver"
+                    marginBottom={10}
+                    onPress={() => goBack()}
+                />
+                <MatButton
+                    title="Agregar Usuario"
+                    buttonColor="#5BB79A"
+                    hoverColor="#3E9C7E"
+                    marginBottom={20}
+                    onPress={guardarActualizarUsuario}
+                />
             </ScrollView>
         </View>
     )
@@ -50,12 +172,43 @@ const styles = StyleSheet.create({
         flex: 1
     },
     scrollContainer: {
-        paddingHorizontal: 10
+        padding: 10
     },
-    accionContainer: {
-        flexDirection: 'row', // Dirección de fila (horizontal)
-        justifyContent: 'space-between', // Espacio uniforme entre los botones
-        paddingHorizontal: 16, // Espacio horizontal alrededor de los botones
+    dropdown: {
+        backgroundColor: '#EDEDED',
+        height: 47,
+        borderColor: '#D3D3D3',
+        borderBottomWidth: 1,
+        borderTopRightRadius: 3,
+        borderTopLeftRadius: 3,
+        paddingHorizontal: 8,
+        marginBottom: 20
+    },
+    icon: {
+        marginRight: 5,
+    },
+    label: {
+        position: 'absolute',
+        backgroundColor: 'white',
+        left: 22,
+        top: 8,
+        zIndex: 999,
+        paddingHorizontal: 8,
+        fontSize: 14,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
     },
 })
 

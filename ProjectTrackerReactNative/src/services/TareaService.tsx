d28a062213789be8fb3536f0a3a565f220549148
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-05-14 08:29:58 -0600

    Servicio de tarea terminado

---
+++ ProjectTrackerReactNative/src/services/TareaService.tsx
@@ -0,0 +1,90 @@
+import { Environment } from "../environments/environment"
+import { ResponseAPI } from "../interfaces/ResponseApiInterface"
+import { Tarea } from "../interfaces/TareaInterface"
+
+class TareaService {
+    urlAPI = Environment.endpoint + "Tarea/"
+
+    // Obtener la lista de tareas registradas
+    async Lista(): Promise<ResponseAPI> {
+
+        // Se realiza la solicitud a la API
+        const response = await fetch(`${this.urlAPI}Lista`, {
+            method: 'GET'
+        })
+
+        // Se retorna la respuesta de la API
+        return response.json()
+    }
+
+    // Obtener la lista de tareas registradas asociadas a un usuario en especifico
+    async ListaUsuario(Id: number): Promise<ResponseAPI> {
+
+        // Se realiza la solicitud a la API
+        const response = await fetch(`${this.urlAPI}ListaUsuario/${Id}`, {
+            method: 'GET'
+        })
+
+        // Se retorna la respuesta de la API
+        return response.json()
+    }
+
+    // Guardar una tarea a la base de datos
+    async Guardar(request: Tarea): Promise<ResponseAPI> {
+        const response = await fetch(`${this.urlAPI}Guardar`, {
+            method: 'POST',
+            body: JSON.stringify({
+                tareId: request.tareId,
+                tareNombre: request.tareNombre,
+                tareDescripcion: request.tareDescripcion,
+                tareFechaInicio: request.tareFechaInicio,
+                tareProyId: request.tareProyId,
+                tareProyNombre: request.tareProyNombre,
+                tareEstaId: request.tareEstaId,
+                tareEstaNombre: request.tareEstaNombre,
+                tareCantidadUsuario: request.tareCantidadUsuario,
+                tareUsuaId: request.tareUsuaId
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+
+        return response.json()
+    }
+
+    // Editar una tarea de la base de datos
+    async Editar(request: Tarea): Promise<ResponseAPI> {
+        const response = await fetch(`${this.urlAPI}Editar`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                tareId: request.tareId,
+                tareNombre: request.tareNombre,
+                tareDescripcion: request.tareDescripcion,
+                tareFechaInicio: request.tareFechaInicio,
+                tareProyId: request.tareProyId,
+                tareProyNombre: request.tareProyNombre,
+                tareEstaId: request.tareEstaId,
+                tareEstaNombre: request.tareEstaNombre,
+                tareCantidadUsuario: request.tareCantidadUsuario,
+                tareUsuaId: request.tareUsuaId
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+
+        return response.json()
+    }
+
+    // Eliminar una tarea registrada
+    async Eliminar(Id: number): Promise<ResponseAPI> {
+        const response = await fetch(`${this.urlAPI}Eliminar/${Id}`, {
+            method: 'DELETE'
+        })
+
+        return response.json()
+    }
+}
+
+export default new TareaService

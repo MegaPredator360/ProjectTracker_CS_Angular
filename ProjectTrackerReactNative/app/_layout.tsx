3cc5056afa7a213e96173415082f1f452e0ed42e
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-05-17 08:18:23 -0600

    Layout de rutas principales

--- ProjectTrackerReactNative/app/_layout.tsx
+++ ProjectTrackerReactNative/app/_layout.tsx
@@ -1,36 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
+
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+  // Si la ruta principal es una carpeta, el app buscará un archivo index
+  // de lo contrario, no cargará
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="(pages)" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+
+        {/* Se utiliza un nombre dinamico para esta pantalla */}
+        <Stack.Screen name="usuarioFormulario" options={({ route }) => ({ title: route.params.name, headerBackTitle: 'Atrás' })} />
+        <Stack.Screen
+          name="usuarioDetalle"
+          options={{
+            title: 'Detalle de Usuario',
+            headerBackTitle: 'Atrás'
+          }} />
       </Stack>
     </ThemeProvider>
   );

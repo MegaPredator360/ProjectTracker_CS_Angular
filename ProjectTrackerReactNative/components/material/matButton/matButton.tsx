e6d67e575fdc42cd77f5584fb8f14fc30b7c344f
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-06-11 10:22:25 -0600

    Cambio de colores a los botones

--- ProjectTrackerReactNative/components/material/matButton/matButton.tsx
+++ ProjectTrackerReactNative/components/material/matButton/matButton.tsx
@@ -1,28 +1,117 @@
 import React, { useState } from 'react';
 import { TouchableHighlight, Text, StyleSheet, GestureResponderEvent, View } from 'react-native';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 type MatButtonProps = {
     title: string;
     onPress?: (((event: GestureResponderEvent) => void) & (() => void)) | undefined;
-    textColor?: string;
-    buttonColor?: string;
-    hoverColor?: string;
+    color?: 'primary' | 'accept' | 'warn' | 'danger'
     marginBottom?: number;
     marginTop?: number;
     marginLeft?: number;
     marginRight?: number;
 };
 
-const MatButton: React.FC<MatButtonProps> = ({ title, onPress, textColor, buttonColor, hoverColor, marginBottom, marginTop, marginLeft, marginRight }) => {
+const MatButton: React.FC<MatButtonProps> = ({ title, onPress, color = 'primary', marginBottom, marginTop, marginLeft, marginRight }) => {
     const [isPressed, setIsPressed] = useState(false); // Estado para rastrear si se está presionando el botón
 
-    const btnColor = buttonColor ? buttonColor : '#2196F3';
-    const btnHover = hoverColor ? hoverColor : '#0B6CBA'
-    const txtColor = textColor ? textColor : '#FFFFFF';
+    // Para el modo oscuro
+    const colorScheme = useColorScheme();
+
     const marginB = marginBottom ? marginBottom : 0;
     const marginT = marginTop ? marginTop : 0;
     const marginL = marginLeft ? marginLeft : 0;
     const marginR = marginRight ? marginRight : 0;
+
+    // Color de boton
+    const getColor = (color: 'primary' | 'accept' | 'warn' | 'danger'): string => {
+        switch (color) {
+
+            // Color primario
+            case 'primary':
+
+                // Modo claro
+                if (colorScheme == 'light') {
+                    return '#5dade2';
+                }
+
+                // Modo oscuro
+                else {
+                    return '#2874a6'
+                }
+
+            // Color de aceptar
+            case 'accept':
+                if (colorScheme == 'light') {
+                    return '#52be80';
+                }
+                else {
+                    return '#1e8449'
+                }
+
+            // Color de Advertencia
+            case 'warn':
+                if (colorScheme == 'light') {
+                    return '#f4d03f';
+                }
+                else {
+                    return '#b7950b'
+                }
+
+            // Color de Peligro
+            case 'danger':
+                if (colorScheme == 'light') {
+                    return '#ec7063';
+                }
+                else {
+                    return '#b03a2e'
+                }
+        }
+    };
+
+    // Color del hover
+    const getColorHover = (color: 'primary' | 'accept' | 'warn' | 'danger'): string => {
+        switch (color) {
+            case 'primary':
+
+                // Modo claro
+                if (colorScheme == 'light') {
+                    return '#3498db';
+                }
+
+                // Modo oscuro
+                else {
+                    return '#21618c'
+                }
+
+            // Color de aceptar
+            case 'accept':
+                if (colorScheme == 'light') {
+                    return '#27ae60';
+                }
+                else {
+                    return '#196f3d'
+                }
+
+            // Color de Advertencia
+            case 'warn':
+                if (colorScheme == 'light') {
+                    return '#f1c40f';
+                }
+                else {
+                    return '#9a7d0a'
+                }
+
+            // Color de peligro
+            case 'danger':
+                if (colorScheme == 'light') {
+                    return '#e74c3c';
+                }
+                else {
+                    return '#943126'
+                }
+        }
+    };
 
     return (
         <TouchableHighlight
@@ -33,13 +122,13 @@ const MatButton: React.FC<MatButtonProps> = ({ title, onPress, textColor, button
             onPress={onPress}
         >
             <View style={[styles.button, {
-                backgroundColor: isPressed ? btnHover : btnColor,
+                backgroundColor: isPressed ? getColorHover(color) : getColor(color),
                 marginBottom: marginB,
                 marginTop: marginT,
                 marginLeft: marginL,
                 marginRight: marginR,
             }]}>
-                <Text style={[styles.text, { color: txtColor }]}>{title}</Text>
+                <Text style={[styles.text]}>{title}</Text>
             </View>
         </TouchableHighlight>
     );
@@ -60,6 +149,7 @@ const styles = StyleSheet.create({
         },
     },
     text: {
+        color: '#FFFFFF',
         fontSize: 16,
         fontWeight: 'bold',
         textAlign: 'center',

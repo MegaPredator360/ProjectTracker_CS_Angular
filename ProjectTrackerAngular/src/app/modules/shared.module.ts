6670fab133443bda96a0134e060be45ef080f1dc
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-19 00:48:26 -0600

    Modulo Compartido agregado

--- ProjectTrackerAngular/src/app/modules/shared.module.ts
+++ ProjectTrackerAngular/src/app/modules/shared.module.ts
@@ -1,10 +1,76 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+// Componetes de Angular Material
+import { MatCardModule } from '@angular/material/card'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatGridListModule } from '@angular/material/grid-list'
+// Contenedores del menu lateral
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+// Recursos globales
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatListModule } from '@angular/material/list'
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatNativeDateModule } from '@angular/material/core'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MomentDateModule } from '@angular/material-moment-adapter'
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search'
+import { MatSortModule } from '@angular/material/sort'
+import { MatMenuModule } from '@angular/material/menu'
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+    MomentDateModule,
+    MatSelectModule,
+    MatSortModule,
+    MatMenuModule,
+    NgxMatSelectSearchModule
+  ],
+  providers: [
+      MatDatepickerModule,
+      MatNativeDateModule
   ]
 })
 

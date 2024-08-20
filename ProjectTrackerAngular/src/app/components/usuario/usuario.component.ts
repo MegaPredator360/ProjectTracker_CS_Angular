d2421eb9c862b7e31aca67ee98897f56326403f5
MegaPredator360 <aaronalfarozamora@gmail.com>
2024-08-19 22:21:38 -0600

    Componente de usuario terminado

--- ProjectTrackerAngular/src/app/components/usuario/usuario.component.ts
+++ ProjectTrackerAngular/src/app/components/usuario/usuario.component.ts
@@ -1,14 +1,15 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Usuario } from '../../interfaces/usuario';
 import { SharedModule } from '../../modules/shared.module';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatSort, Sort } from '@angular/material/sort';
 import { UsuarioService } from '../../services/usuario.service';
-import { Router } from '@angular/router';
 import { UtilityService } from '../../services/utility.service';
 import { MatDialog } from '@angular/material/dialog';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { ModalUsuarioComponent } from './modal-usuario/modal-usuario.component';
+import { ModalBorrarUsuarioComponent } from './modal-borrar-usuario/modal-borrar-usuario.component';
 
 @Component({
   selector: 'app-usuario',
@@ -17,17 +18,14 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.scss'
 })
-export class UsuarioComponent {
+export class UsuarioComponent implements OnInit, AfterViewInit {
   colunmasTabla: string[] = ['usuaCedula', 'usuaNombre', 'usuaCorreo', 'usuaTelefono', 'usuaPermNombre', 'acciones']
-  dataInicio: Usuario[] = []
-  dataListaUsuarios = new MatTableDataSource(this.dataInicio)
-  usuarioVacio!: Usuario
+  dataListaUsuarios = new MatTableDataSource<Usuario[]>
   @ViewChild(MatPaginator) paginacionTabla!: MatPaginator
   @ViewChild(MatSort) sortTabla!: MatSort
   mensajeVacio: string = "No hay usuarios registrados";
 
   constructor(
-    private router: Router,
     private usuarioService: UsuarioService,
     private utilityService: UtilityService,
     private dialog: MatDialog,
@@ -44,7 +42,10 @@ export class UsuarioComponent {
           this.dataListaUsuarios.data = data.value
         }
       },
-      //error: (e) => { this.utilityService.mostrarAlerta("Ocurrio un error al obtener los usuarios", "error") }
+      error: (e) => { 
+        this.utilityService.mostrarAlerta("Ocurrio un error al obtener los usuarios", "error")
+        console.error(e)
+      }
     })
   }
 
@@ -80,14 +81,30 @@ export class UsuarioComponent {
     }
   }
 
-  /*
-  usuarioEliminar(usuario: Usuario) {
-    this.dialog.open(UsuarioModalComponent, {
+  usuarioNuevo() {
+    this.dialog.open(ModalUsuarioComponent, {
+      disableClose: true,
+      data: null
+    }).afterClosed().subscribe(() => {
+      this.obtenerUsuarios();
+    });
+  }
+
+  usuarioActualizar(usuario: Usuario) {
+    this.dialog.open(ModalUsuarioComponent, {
       disableClose: true,
       data: usuario
-    }).afterClosed().subscribe(resultado => {
+    }).afterClosed().subscribe(() => {
+      this.obtenerUsuarios();
+    });
+  }
+
+  usuarioEliminar(usuario: Usuario) {
+    this.dialog.open(ModalBorrarUsuarioComponent, {
+      disableClose: true,
+      data: usuario
+    }).afterClosed().subscribe(() => {
       this.obtenerUsuarios()
     });
   }
-    */
 }
